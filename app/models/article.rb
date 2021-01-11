6886bfd2b8c5f1d57a11a310f3ed4ed2c1dc93a0
yt1013 <yutaka.tsugu@asia-quest.jp>
2021-01-11 18:44:36 +0900

    コメントコントローラーの追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,10 @@
 class Article < ApplicationRecord
+    has_many :comments
     validates :title,
         presence: true,
         uniqueness: true,
         length: { minimum: 5 }
-        
+
     validates :text,
         presence: true
 end
