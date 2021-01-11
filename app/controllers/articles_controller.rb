fe34aea46addbd62f7fe0cb60cb8248c3cd21723
yt1013 <yutaka.tsugu@asia-quest.jp>
2021-01-11 16:09:51 +0900

    編集機能の実装

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,6 +7,10 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def edit
+        @article = Article.find(params[:id])
+    end
+
     def new
         @article = Article.new
     end
@@ -18,6 +22,16 @@ class ArticlesController < ApplicationController
             redirect_to @article
         else
             render 'new'
+        end
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            redirect_to @article
+        else
+            render 'edit'
         end
     end
 
